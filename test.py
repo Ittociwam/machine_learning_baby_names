8cc589debf20618933f0f28fb09c02619bd0c7a2
Robbie <ittociwam@hotmail.com>
2016-03-23 14:10:06 -0600

    min functionality finished
    Signed-off-by: Robbie <ittociwam@hotmail.com>

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import tmdbsimple as tmdb
+import re
 import json
 import requests
 
@@ -26,57 +27,100 @@ import requests
 # 			for baby_names in file
 # 				is baby_name in name
 
-year = 2005
-x = 0
-# Read in the csv and put the names and their counts for that year into a dict
-for i_year in (range(year - x, year + (x+ 1))):
-    path = os.getcwd()
-    file_path = path + '/names/yob' + str(i_year) + '.csv'
-    if os.path.exists(file_path):
-        print("found", i_year, "file")
-        file = open(file_path, 'r')
-        names = csv.reader(file)
-        baby_names = {}
-        for i, name in enumerate(names):
-            baby_names[name[0]]= name[2]
-            print(name)
+class MovieNameStudy:
+
+    def __init__(self, year, span, pages):
+        tmdb.API_KEY = 'e026cff454ca0e6e446e959e87c05bcf'
+        self.year = year
+        self.span = span
+        self.pages = pages
+        self.name_results = {}
+        self.get_names()
+
+
+        self.get_cast_names()
+        for i in self.name_results:
+            if self.name_results[i].movies:
+                print(i)
+                for j in self.name_results[i].occurances:
+                    print("\t", j, self.name_results[i].occurances[j])
+                print("MOVIES: ", self.name_results[i].movies)
+                for k in self.name_results[i].movies:
+                    print(k)
+                    print("\t", k, self.name_results[i].movies[k])
+
+    class Name:
+        def __init__(self, name, year, num):
+            self.name = name
+            self.occurances = {year: num}
+            self.movies = {}
+
+        def add_year(self, add_year, num):
+            if add_year in self.occurances:
+                self.occurances[add_year] = int(self.occurances[add_year]) + int(num)
+            else:
+                self.occurances[add_year] = num
+
+
+    def count_instances(self, movie_name, names_list):
+        # Store the names and num of babies born with that name for every name that showed up in the movie
+        names_dict = {}
+        for name in names_list:
+            try:
+                self.name_results[name].movies[movie_name] = self.year
+            except KeyError:
+                print(name, "not used")
+
+    # Read in the csv and put the names and their counts for that year into a dict
+    def get_names(self):
+        for i_year in (range(self.year - self.span, self.year + (self.span+ 1))):
+            path = os.getcwd()
+            file_path = path + '/names/yob' + str(i_year) + '.csv'
+            if os.path.exists(file_path):
+                print("found", i_year, "file")
+                file = open(file_path, 'r')
+                names = csv.reader(file)
+                for i, name in enumerate(names):
+                    if name[0] in self.name_results:
+                        self.name_results[name[0]].add_year(i_year, name[2])
+                    else:
+                        self.name_results[name[0]] = self.Name(name[0], i_year, name[2])
+                    #print(name)
+
+
+
+    def has_numbers(self, input):
+        return bool(re.search(r'\d', input))
+
+
+    def get_cast_names(self):
+        # Get the  top movies for a year (10 per page)
+        kwargs = {'primary_release_year':self.year, 'page':'1'}
+        disc_obj = tmdb.Discover()
+        top = disc_obj.movie(**kwargs)
+        movies = top['results']
+        movie_ids = []
+        for item in movies:
+            movie_ids.append(item['id'])
+        for id in movie_ids:
+            movie = tmdb.Movies(id)
+            response = movie.info()
+            print(movie.title)
+            cast = movie.credits()['cast']
+            names_list = []
+            for value in cast:
+                if not self.has_numbers(value['character']):
+                    for n in value['character'].split(' '):
+                        if n == '(voice)' or n is '/' or n is '|':
+                            continue
+                        names_list.append(n)
+                for n in value['name'].split(' '):
+                    names_list.append(n)
+            print(names_list)
+            self.count_instances(movie.title, names_list)
 
 
 
 
-tmdb.API_KEY = 'e026cff454ca0e6e446e959e87c05bcf'
 
-
-
-# Get the  top movies for a year (10 per page)
-kwargs = {'primary_release_year':'2010', 'page':'2'}
-disc_obj = tmdb.Discover()
-top = disc_obj.movie(**kwargs)
-movies = top['results']
-movie_ids = []
-for item in movies:
-    movie_ids.append(item['id'])
-for id in movie_ids:
-    print(id)
-
-# get the names of cast and characters
-names_list = []
-movie = tmdb.Movies(603)
-response = movie.info()
-print(movie.title)
-cast = movie.credits()['cast']
-for value in cast:
-    names_list.append(value['character'])
-    names_list.append(value['name'])
-    print (value['character'], '-', value['name'])
-
-# Store the names and num of babies born with that name for every name that showed up in the movie
-names_dict = {}
-for baby_name in baby_names:
-    if baby_name in names_list:
-        try:
-            names_dict[baby_name] = names_dict[baby_name] + baby_names[baby_name]
-        except KeyError:
-            names_dict[baby_name] = baby_names[baby_name]
-for i in names_dict.items():
-    print (i)
+MovieNameStudy(1975, 5, 3)
